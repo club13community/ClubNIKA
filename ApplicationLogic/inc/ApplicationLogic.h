1f5dc7f8eb25eb124948d9d98c4ef5b8b3a4bd1c
Maksym Stasiuk <maxstasiuk92@gmail.com>
2020-05-06 02:06:09 +0300

    first clean commit

---
+++ ApplicationLogic/inc/ApplicationLogic.h
@@ -0,0 +1,13 @@
+/*
+ * ApplicationLogic.h
+ *
+ *  Created on: 6 трав. 2020 р.
+ *      Author: MaxCm
+ */
+
+#ifndef _APPLICATIONLOGIC_H_
+#define _APPLICATIONLOGIC_H_
+
+
+
+#endif /* _APPLICATIONLOGIC_H_ */
